ddf84453ce89865273d93f4792d49d050c3b7580
NsavelevJs <nsavelevjs@gmail.com>
2022-09-20 14:11:46 -0400

    added background

--- native-app/App.js
+++ native-app/App.js
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components/native';
-
-
+import { StatusBar } from "expo-status-bar";
+import { Button, ImageBackground, StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
+const image = {uri: "https://i.gifer.com/embedded/download/76YS.gif"}
 export default function App() {
   return (
     <View style={styles.container}>
-      <styleHeader>SUP</styleHeader>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={image} style={styles.image}>
+      <View>
+        <Text style={styles.mono}>The Endless Void of Space</Text>
+      </View>
+      <Button style={styles.button} title="Do Nothing Button!" />
+      </ImageBackground>
     </View>
   );
 }
@@ -16,13 +18,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button:{
+    padding:5,
+  },
+  mono: {
+    fontFamily:"monospace",
+    margin:16,
+    fontSize: 20,
+    color: "green",
+    backgroundColor: "#000000c0",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
   },
 });
-
-const styleHeader = styled.Text`
-
-`
-
